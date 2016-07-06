3852c5e5e80c396db0250d2c9105ad78e393f944
Piyush Soni <piyush_soni@yahoo.com>
2016-07-06 09:02:10 +0530

    Add new website, more changes
    - The websites are changing their adblock pretty frequently. Make it more reliable for these cases.

--- Block-Adblocker-blocker.user.js
+++ Block-Adblocker-blocker.user.js
@@ -1,15 +1,16 @@
-﻿// ==UserScript==
+// ==UserScript==
 // @name        Block-Adblocker-blocker
 // @namespace   http://www.piyushsoni.com
 // @description Blocks Anti-adblockers on several Indian News Sites
 // @include     http://timesofindia.indiatimes.com/*
 // @include     http://www.hindustantimes.com/*
 // @include     http://*.ndtv.com/*
-// @run-at		document-start
-// @version     1.10
+// @include     http://*.anandabazar.com/*
+// @run-at      document-start
+// @version     1.20
 // @grant       none
-// @author		Piyush Soni (irrationalapps+blockerblocker@gmail.com)
-// @license		GNU LGPL v3 (https://www.gnu.org/licenses/lgpl-3.0.html)
+// @author      Piyush Soni (irrationalapps+blockerblocker@gmail.com)
+// @license     GNU LGPL v3 (https://www.gnu.org/licenses/lgpl-3.0.html)
 // ==/UserScript==
 
 var url = window.location.href.toLowerCase();
@@ -17,15 +18,15 @@ var domain = new URL(url).hostname.replace("www.","");
 var mapPagesBlockerBlockers = {};
 var countStopped = 0;
 var countToCheck = 0;
-var inlineBlocker = new String("");
-var externalBlocker = new String("");
+var arrPageBlockers;
 
 
 //Add new blocker list here
 ///mapPagesBlockerBlockers['<domain>'] = ['<external script source match>','<inline script source match>', <stop any 1 r more(count)>]
-mapPagesBlockerBlockers['indiatimes.com'] = ['detector/minify-1.cms', 'blocked.cms', 1];
-mapPagesBlockerBlockers['hindustantimes.com'] = ['BlockerScript', 'checkAdBlocker', 2];
-mapPagesBlockerBlockers['ndtv.com'] = ['', 'canRunAds', 1];
+mapPagesBlockerBlockers['indiatimes.com'] = [['detector', 'blocked.cms'], 1];
+mapPagesBlockerBlockers['hindustantimes.com'] = [['BlockerScript', 'checkAdBlocker'], 2];
+mapPagesBlockerBlockers['ndtv.com'] = [['canRunAds'], 1];
+mapPagesBlockerBlockers['anandabazar.com'] = [['custom.js', 'checkAdBlock'], 2];
 
 
 
@@ -35,28 +36,33 @@ if(arrDomain.length > 2)
 
 function scriptListener(e)
 {
-	let src = e.target.src;
-	if ((src && externalBlocker.length > 0 && src.indexOf(externalBlocker) >= 0) || (inlineBlocker.length > 0 && e.target.innerHTML.indexOf(inlineBlocker) >= 0)) 
-	{
-		//Stop this nonsense. 
-		e.preventDefault();
-		e.stopPropagation();
-		
-		console.log("Blocked an Anti-adblocker for " + domain);
-
-		countStopped++;
-		if(countStopped >= countToCheck)
+    let i = 0;
+    let src = e.target.src;
+    let html = e.target.innerHTML;
+    for(i=0; i < arrPageBlockers.length; ++i)
+    {
+        let blocker = arrPageBlockers[i];
+        if(blocker.length > 0 && ((typeof src != "undefined" && src && src.indexOf(blocker) >= 0) || (typeof html != "undefined" && html && html.indexOf(blocker) >= 0)))
 		{
-			//We're done, remove the event listener now
-			window.removeEventListener(e.type, arguments.callee, true);
+			//Stop this nonsense. 
+			e.preventDefault();
+			e.stopPropagation();
+			
+			console.log("Blocked an Anti-adblocker for " + domain);
+
+			countStopped++;
+			if(countStopped >= countToCheck)
+			{
+				//We're done, remove the event listener now
+				window.removeEventListener(e.type, arguments.callee, true);
+			}
 		}
-	}
+    }
 }
 
 if(domain in mapPagesBlockerBlockers)
 {
-	externalBlocker = mapPagesBlockerBlockers[domain][0];
-	inlineBlocker = mapPagesBlockerBlockers[domain][1];
-	countToCheck = mapPagesBlockerBlockers[domain][2];	
+	arrPageBlockers = mapPagesBlockerBlockers[domain][0];
+	countToCheck = mapPagesBlockerBlockers[domain][1];
 	window.addEventListener('beforescriptexecute', scriptListener, true);
 }
